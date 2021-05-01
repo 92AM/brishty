1cb62ccff3c0b7ed3c81b52347869c7f4ce2b6f1
Arka Mitra <arka2k8@gmail.com>
2021-05-02 00:26:41 +0100

    Refactoring moving Nav inside Header component, it doesn't need to be outside

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import SearchLocation from "./SearchLocation";
-import Nav from "./Nav";
 
 const Title = () => {
     return (
@@ -16,6 +15,35 @@ const Title = () => {
         </div>
     );
 }
+
+const exampleWeatherApiExample: string = "/api/weather?location=london";
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul className="list-reset md:flex md:items-center md:space-x-10">
+                <li>
+                    <a className="block no-underline hover:underline py-2 text-white md:border-none md:p-0"
+                       href={exampleWeatherApiExample}>
+                        API Example
+                    </a>
+                </li>
+                <li>
+                    <a className="border-t block no-underline hover:underline py-2 text-white md:border-none md:p-0"
+                       href="#">
+                        About
+                    </a>
+                </li>
+                <li>
+                    <a className="border-t block no-underline hover:underline py-2 text-white md:border-none md:p-0"
+                       href="#">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
 
 const Header = () => {
     return (
