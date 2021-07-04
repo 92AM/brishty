3f6f4c22bc9d3bed777c527ae3f2ac5414a0b864
Arka Mitra <arka2k8@gmail.com>
2021-07-04 22:10:35 +0100

    Minor style changes

--- components/TodaysWeatherDescription.tsx
+++ components/TodaysWeatherDescription.tsx
@@ -8,7 +8,7 @@ type Props = {
 const TodaysWeatherDescription = ({item: weatherDetails}: Props) => {
     return (
         <div className="2xl:px-32">
-            <div className="text-gray-800 text-2xl text-left p-2">
+            <div className="text-gray-800 text-2xl text-left p-2 pb-4">
                 Today's weather forecast :
             </div>
             <div className="text-gray-800 text-lg text-left p-2">
