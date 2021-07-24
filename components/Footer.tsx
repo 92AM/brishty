5fb262c8a7b7a39491add3a825d640e6784fcb15
Arka Mitra <arka2k8@gmail.com>
2021-07-24 23:52:13 +0100

    Updates footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+const currentYear = new Date().getFullYear();
+
 const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white text-2l text-center p-8">
-            Brishty &#169; 2021
+            <div className="flex flex-col">
+                <span>{`Brishty © ${currentYear}`}</span>
+                <span className="pt-2">
+                    Created by <a className="hover:underline" href={"https://www.arkamitra.com"}>Arka Mitra</a>
+                </span>
+            </div>
         </footer>
     );
 };
