ba0a669b39227fa0d75181de209b0b2955c5c94b
Arka Mitra <arka2k8@gmail.com>
2021-08-06 23:52:19 +0100

    Fixes build and adds test as part of npm run build

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts',],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-    testMatch: [
-        '**/?(*.)spec.ts?(x)'
-    ],
+    testMatch: ['**/?(*.)spec.ts?(x)'],
     testEnvironment: 'jsdom',
 };
