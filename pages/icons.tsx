f462c0f30d54fbb9f9edab710d2722974c4b86d3
Arka Mitra <arka2k8@gmail.com>
2021-08-08 14:52:05 +0100

    Procrastinating - updated icon page styling

--- pages/icons.tsx
+++ pages/icons.tsx
@@ -9,10 +9,13 @@ type Props = {
 };
 
 const Icon = ({ description, value }: Props) => {
+    const cx = description.indexOf('night') >= 0 ? 'bg-gray-300' : 'bg-white';
     return (
-        <div className="flex justify-center py-6">
-            <img className="h-24 w-24 pt-2" src={value} alt={value} />
-            <span className="pt-10 py-2 text-gray-800 text-lg">{description}</span>
+        <div className="py-6">
+            <div className={'flex shadow-lg border border-gray-600 rounded-3xl justify-center ' + cx}>
+                <img className="h-24 w-24 pt-2" src={value} alt={value} />
+                <span className="pt-10 py-2 text-gray-800 text-lg">{description}</span>
+            </div>
         </div>
     );
 };
@@ -33,57 +36,54 @@ export default function Icons() {
                     </span>
                     <span className="text-blue-900 hover:underline text-xl">
                         <Link href={'https://openweathermap.org/weather-conditions'}>
-                            {'Open Weather Map Page Icon List >'}
+                            {'Open Weather Map >'}
                         </Link>
                     </span>
                 </div>
 
-                <div className="bg-white text-white text-2l text-center">
-                    <div
-                        className="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-screen-xl lg:px-8
-                divide-y divide-gray-500 lg:divide-y-0"
-                    >
-                        <div className="lg:grid lg:grid-cols-2 lg:gap-9 divide-y divide-gray-500 lg:divide-y-0">
+                <div className="py-6 bg-white text-white text-2l text-center">
+                    <div className="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-screen-xl lg:px-8">
+                        <div className="lg:grid lg:grid-cols-2 lg:gap-9">
                             <Icon description={'Clear sky (day icon)'} value={getIconSourceLink('01d')} />
                             <Icon description={'Clear sky (night icon)'} value={getIconSourceLink('01n')} />
                         </div>
 
-                        <div className="lg:grid lg:grid-cols-2 lg:gap-9 divide-y divide-gray-500 lg:divide-y-0">
+                        <div className="lg:grid lg:grid-cols-2 lg:gap-9">
                             <Icon description={'Few clouds (day icon)'} value={getIconSourceLink('02d')} />
                             <Icon description={'Few clouds (night icon)'} value={getIconSourceLink('02n')} />
                         </div>
 
-                        <div className="lg:grid lg:grid-cols-2 lg:gap-9 divide-y divide-gray-500 lg:divide-y-0">
+                        <div className="lg:grid lg:grid-cols-2 lg:gap-9">
                             <Icon description={'Scattered clouds (day icon)'} value={getIconSourceLink('03d')} />
                             <Icon description={'Scattered clouds (night icon)'} value={getIconSourceLink('03n')} />
                         </div>
 
-                        <div className="lg:grid lg:grid-cols-2 lg:gap-9 divide-y divide-gray-500 lg:divide-y-0">
+                        <div className="lg:grid lg:grid-cols-2 lg:gap-9">
                             <Icon description={'Broken clouds (day icon)'} value={getIconSourceLink('04d')} />
                             <Icon description={'Broken clouds (night icon)'} value={getIconSourceLink('04n')} />
                         </div>
 
-                        <div className="lg:grid lg:grid-cols-2 lg:gap-9 divide-y divide-gray-500 lg:divide-y-0">
+                        <div className="lg:grid lg:grid-cols-2 lg:gap-9">
                             <Icon description={'Shower rain (day icon)'} value={getIconSourceLink('09d')} />
                             <Icon description={'Shower rain (night icon)'} value={getIconSourceLink('09n')} />
                         </div>
 
-                        <div className="lg:grid lg:grid-cols-2 lg:gap-9 divide-y divide-gray-500 lg:divide-y-0">
+                        <div className="lg:grid lg:grid-cols-2 lg:gap-9">
                             <Icon description={'Rain (day icon)'} value={getIconSourceLink('10d')} />
                             <Icon description={'Rain rain (night icon)'} value={getIconSourceLink('10n')} />
                         </div>
 
-                        <div className="lg:grid lg:grid-cols-2 lg:gap-9 divide-y divide-gray-500 lg:divide-y-0">
+                        <div className="lg:grid lg:grid-cols-2 lg:gap-9">
                             <Icon description={'Thunderstorm (day icon)'} value={getIconSourceLink('11d')} />
                             <Icon description={'Thunderstorm (night icon)'} value={getIconSourceLink('11n')} />
                         </div>
 
-                        <div className="lg:grid lg:grid-cols-2 lg:gap-9 divide-y divide-gray-500 lg:divide-y-0">
+                        <div className="lg:grid lg:grid-cols-2 lg:gap-9">
                             <Icon description={'Snow (day icon)'} value={getIconSourceLink('13d')} />
                             <Icon description={'Snow (night icon)'} value={getIconSourceLink('13d')} />
                         </div>
 
-                        <div className="lg:grid lg:grid-cols-2 lg:gap-9 divide-y divide-gray-500 lg:divide-y-0">
+                        <div className="lg:grid lg:grid-cols-2 lg:gap-9">
                             <Icon description={'Mist (day icon)'} value={getIconSourceLink('50d')} />
                             <Icon description={'Mist (night icon)'} value={getIconSourceLink('50n')} />
                         </div>
