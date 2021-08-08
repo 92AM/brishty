e08f511a6a658ece074e019a446cf54025ada8b5
Arka Mitra <arka2k8@gmail.com>
2021-08-08 02:52:05 +0100

    Refactoring code style

--- components/WeatherMapLegend.tsx
+++ components/WeatherMapLegend.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import CSS from 'csstype';
 
-const windGradientStyle: CSS.Properties = {
-    background:
-        'linear-gradient(to left, ' +
-        'rgb(158, 128, 177), ' +
-        'rgba(116, 76, 172, 0.9), ' +
-        'rgb(164, 123, 170), ' +
-        'rgba(170, 128, 177, 0.84), ' +
-        'rgba(176, 128, 177, 0.71), ' +
-        'rgba(170, 128, 177, 0.54), ' +
-        'rgba(170, 128, 177, 0.44), ' +
-        'rgba(255, 255, 0, 0))',
-};
-
 const WindIndex = () => {
+    const windGradientStyle: CSS.Properties = {
+        background:
+            'linear-gradient(to left, ' +
+            'rgb(158, 128, 177), ' +
+            'rgba(116, 76, 172, 0.9), ' +
+            'rgb(164, 123, 170), ' +
+            'rgba(170, 128, 177, 0.84), ' +
+            'rgba(176, 128, 177, 0.71), ' +
+            'rgba(170, 128, 177, 0.54), ' +
+            'rgba(170, 128, 177, 0.44), ' +
+            'rgba(255, 255, 0, 0))',
+    };
+
     return (
         <div className="p-2 w-60 bg-gray-800 text-gray-100 rounded-2xl">
             <span className="p-1 text-sm">Wind speed, m/s</span>
@@ -35,12 +35,29 @@ const WindIndex = () => {
     );
 };
 
-const temperatureGradientStyle: CSS.Properties = {
-    background:
-        'linear-gradient(to right, rgb(159, 85, 181) 0%, rgb(44, 106, 187) 8.75%, rgb(82, 139, 213) 12.5%, rgb(103, 163, 222) 18.75%, rgb(142, 202, 240) 25%, rgb(155, 213, 244) 31.25%, rgb(172, 225, 253) 37.5%, rgb(194, 234, 255) 43.75%, rgb(255, 255, 208) 50%, rgb(254, 248, 174) 56.25%, rgb(254, 232, 146) 62.5%, rgb(254, 226, 112) 68.75%, rgb(253, 212, 97) 75%, rgb(244, 168, 94) 82.5%, rgb(244, 129, 89) 87.5%, rgb(244, 104, 89) 93.75%, rgb(244, 76, 73) 100%)',
-};
-
 const TemperatureIndex = () => {
+    const temperatureGradientStyle: CSS.Properties = {
+        background:
+            'linear-gradient(to right, ' +
+            'rgb(159, 85, 181) 0%, ' +
+            'rgb(44, 106, 187) 8.75%, ' +
+            'rgb(82, 139, 213) 12.5%, ' +
+            'rgb(103, 163, 222) 18.75%, ' +
+            'rgb(142, 202, 240) 25%, ' +
+            'rgb(155, 213, 244) 31.25%, ' +
+            'rgb(172, 225, 253) 37.5%, ' +
+            'rgb(194, 234, 255) 43.75%, ' +
+            'rgb(255, 255, 208) 50%, ' +
+            'rgb(254, 248, 174) 56.25%, ' +
+            'rgb(254, 232, 146) 62.5%, ' +
+            'rgb(254, 226, 112) 68.75%, ' +
+            'rgb(253, 212, 97) 75%, ' +
+            'rgb(244, 168, 94) 82.5%, ' +
+            'rgb(244, 129, 89) 87.5%, ' +
+            'rgb(244, 104, 89) 93.75%, ' +
+            'rgb(244, 76, 73) 100%)',
+    };
+
     return (
         <div className="p-2 w-60 bg-gray-800 text-gray-100 rounded-2xl">
             <span className="p-1 text-sm">Temperature, °C</span>
@@ -58,21 +75,21 @@ const TemperatureIndex = () => {
     );
 };
 
-const pressureGradientStyle: CSS.Properties = {
-    background:
-        'linear-gradient(to right, ' +
-        'rgb(0, 115, 255) 0%, ' +
-        'rgb(0, 170, 255) 8.35059%, ' +
-        'rgb(75, 208, 214) 24.9192%, ' +
-        'rgb(141, 231, 199) 41.4879%, ' +
-        'rgb(176, 247, 32) 49.7722%, ' +
-        'rgb(240, 184, 0) 58.0565%, ' +
-        'rgb(251, 85, 21) 74.6251%, ' +
-        'rgb(243, 54, 59) 91.1938%, ' +
-        'rgb(198, 0, 0) 100%)',
-};
-
 const PressureIndex = () => {
+    const pressureGradientStyle: CSS.Properties = {
+        background:
+            'linear-gradient(to right, ' +
+            'rgb(0, 115, 255) 0%, ' +
+            'rgb(0, 170, 255) 8.35059%, ' +
+            'rgb(75, 208, 214) 24.9192%, ' +
+            'rgb(141, 231, 199) 41.4879%, ' +
+            'rgb(176, 247, 32) 49.7722%, ' +
+            'rgb(240, 184, 0) 58.0565%, ' +
+            'rgb(251, 85, 21) 74.6251%, ' +
+            'rgb(243, 54, 59) 91.1938%, ' +
+            'rgb(198, 0, 0) 100%)',
+    };
+
     return (
         <div className="p-2 w-60 bg-gray-800 text-gray-100 rounded-2xl">
             <span className="p-1 text-sm">Pressure, hPa</span>
@@ -90,25 +107,25 @@ const PressureIndex = () => {
     );
 };
 
-const cloudGradientStyle: CSS.Properties = {
-    background:
-        'linear-gradient(to right, ' +
-        'rgba(247, 247, 255, 0) 0%, ' +
-        'rgba(251, 247, 255, 0) 10%, ' +
-        'rgba(244, 248, 255, 0.1) 20%, ' +
-        'rgba(240, 249, 255, 0.2) 30%, ' +
-        'rgba(221, 250, 255, 0.4) 40%, ' +
-        'rgba(224, 224, 224, 0.9) 50%, ' +
-        'rgba(224, 224, 224, 0.76) 60%, ' +
-        'rgba(228, 228, 228, 0.9) 70%, ' +
-        'rgba(232, 232, 232, 0.9) 80%, ' +
-        'rgb(214, 213, 213) 90%, ' +
-        'rgb(210, 210, 210) 95%, ' +
-        'rgb(183, 183, 183) ' +
-        '100%)',
-};
-
 const CloudIndex = () => {
+    const cloudGradientStyle: CSS.Properties = {
+        background:
+            'linear-gradient(to right, ' +
+            'rgba(247, 247, 255, 0) 0%, ' +
+            'rgba(251, 247, 255, 0) 10%, ' +
+            'rgba(244, 248, 255, 0.1) 20%, ' +
+            'rgba(240, 249, 255, 0.2) 30%, ' +
+            'rgba(221, 250, 255, 0.4) 40%, ' +
+            'rgba(224, 224, 224, 0.9) 50%, ' +
+            'rgba(224, 224, 224, 0.76) 60%, ' +
+            'rgba(228, 228, 228, 0.9) 70%, ' +
+            'rgba(232, 232, 232, 0.9) 80%, ' +
+            'rgb(214, 213, 213) 90%, ' +
+            'rgb(210, 210, 210) 95%, ' +
+            'rgb(183, 183, 183) ' +
+            '100%)',
+    };
+
     return (
         <div className="p-2 w-60 bg-gray-800 text-gray-100 rounded-2xl">
             <span className="p-1 text-sm">Clouds, %</span>
@@ -126,19 +143,19 @@ const CloudIndex = () => {
     );
 };
 
-const precipitationGradientStyle: CSS.Properties = {
-    background:
-        'linear-gradient(to left, ' +
-        'rgba(20, 20, 255, 0.9), ' +
-        'rgba(80,80, 225, 0.7), ' +
-        'rgba(110, 110, 205, 0.3), ' +
-        'rgba(120, 120, 190, 0), ' +
-        'rgba(150, 150, 170, 0), ' +
-        'rgba(200, 150, 150, 0), ' +
-        'rgba(225, 200, 100, 0))',
-};
-
 const PrecipitationIndex = () => {
+    const precipitationGradientStyle: CSS.Properties = {
+        background:
+            'linear-gradient(to left, ' +
+            'rgba(20, 20, 255, 0.9), ' +
+            'rgba(80,80, 225, 0.7), ' +
+            'rgba(110, 110, 205, 0.3), ' +
+            'rgba(120, 120, 190, 0), ' +
+            'rgba(150, 150, 170, 0), ' +
+            'rgba(200, 150, 150, 0), ' +
+            'rgba(225, 200, 100, 0))',
+    };
+
     return (
         <div className="p-2 w-60 bg-gray-800 text-gray-100 rounded-2xl">
             <span className="p-1 text-sm">Precipitation, mm</span>
