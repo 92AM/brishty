d275e5e2402ff41b995ba19729cea10f92adc70c
Arka Mitra <arka2k8@gmail.com>
2021-08-20 20:51:20 +0100

    Minor map tweaking

--- pages/_app.js
+++ pages/_app.js
@@ -3,5 +3,5 @@ import '../styles/globals.css';
 import '../styles/customStyles.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
