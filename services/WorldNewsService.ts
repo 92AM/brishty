3ec1949e1f23fee43c45baac403718a53de311f2
Arka Mitra <arka2k8@gmail.com>
2021-08-22 00:57:53 +0100

    Attempting to break brishty

--- services/WorldNewsService.ts
+++ services/WorldNewsService.ts
@@ -1,31 +1,37 @@
 import { News } from '../interfaces';
 import { nyTimesNewsClient } from '../api/NyTimesNewsClient';
 
+const isMultimediaAvailable = (news: any): boolean => {
+    return news.multimedia !== null && news.multimedia !== undefined;
+};
+
+const mapNewsJsonToNews = (nearbyLocationsAsJson: any) => {
+    const newsResults = nearbyLocationsAsJson.results;
+    const mappedNewsItems: News[] = [];
+
+    newsResults.forEach((news: any) => {
+        const tempNews: News = {
+            section: news.section,
+            subsection: news.subsection,
+            title: news.title,
+            newsDescription: news.abstract,
+            byLine: news.byline,
+            publishedDate: new Date(news.published_date).toDateString(),
+            imageSource: news.multimedia[0]?.url,
+            // imageSource: isMultimediaAvailable(news) ? news.multimedia[0]?.url : '/images/placeholder-large.jpg',
+            imageCaption: isMultimediaAvailable(news) ? news.multimedia[0]?.caption : 'image caption unavailable',
+            imageCopyright: isMultimediaAvailable(news)
+                ? news.multimedia[0]?.copyright
+                : 'image copyright information unavailable',
+            facets: [...news.geo_facet, ...news.org_facet, ...news.per_facet],
+        };
+        mappedNewsItems.push(tempNews);
+    });
+
+    return mappedNewsItems;
+};
+
 export const getWorldNews = async (): Promise<News[]> => {
     const nearbyLocationsAsJson = await nyTimesNewsClient();
-
-    const mapNewsJsonToNews = (nearbyLocationsAsJson: any) => {
-        const newsResults = nearbyLocationsAsJson.results;
-        const mappedNewsItems: News[] = [];
-
-        newsResults.forEach((news: any) => {
-            const tempNews: News = {
-                section: news.section,
-                subsection: news.subsection,
-                title: news.title,
-                newsDescription: news.abstract,
-                byLine: news.byline,
-                publishedDate: new Date(news.published_date).toDateString(),
-                imageSource: news.multimedia[0].url,
-                imageCaption: news.multimedia[0].caption,
-                imageCopyright: news.multimedia[0].copyright,
-                facets: [...news.geo_facet, ...news.org_facet, ...news.per_facet],
-            };
-            mappedNewsItems.push(tempNews);
-        });
-
-        return mappedNewsItems;
-    };
-
     return mapNewsJsonToNews(nearbyLocationsAsJson);
 };
