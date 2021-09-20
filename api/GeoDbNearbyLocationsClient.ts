7759aedae088c0653b0b0eb1c102e3633b14a5bc
Arka Mitra <arka2k8@gmail.com>
2021-09-20 09:14:20 +0100

    Pushing updates

--- api/GeoDbNearbyLocationsClient.ts
+++ api/GeoDbNearbyLocationsClient.ts
@@ -15,14 +15,15 @@ export const geoDbNearbyLocationsClient = async (
     // 24 hours
     const cacheForInHours = 24;
     const cacheExpiryInMillisecond = cacheForInHours * 1000 * 60 * 60;
-
-    const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/locations/${latitude}${longitude}/nearbyCities?radius=${radius}&limit=${limit}&countryIds=${countryCode}&types=${type}`;
-
-    return await fetchResponseFromClientOrCache(url, cacheExpiryInMillisecond, {
+    const requestInit = {
         method: 'GET',
         headers: {
             'x-rapidapi-key': GEO_DB_CITIES_API_KEY,
             'x-rapidapi-host': X_RAPID_API_HOST_VALUE,
         },
-    });
+    };
+
+    const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/locations/${latitude}${longitude}/nearbyCities?radius=${radius}&limit=${limit}&countryIds=${countryCode}&types=${type}`;
+
+    return await fetchResponseFromClientOrCache(url, cacheExpiryInMillisecond, requestInit);
 };
--- pages/api/weather/index.ts
+++ pages/api/weather/index.ts
@@ -29,6 +29,8 @@ export const handler: (_req: NextApiRequest, res: NextApiResponse) => Promise<vo
         }
         res.status(200).json(result);
     } catch (err) {
+        console.log('HELLO!!! ==>');
+
         res.status(500).json({ statusCode: 500, message: err.message });
     }
 };
