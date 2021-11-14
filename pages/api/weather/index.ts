ed2afff88f366472745ad72e6e26ca4f5d7e7eec
Arka Mitra <arka2k8@gmail.com>
2021-11-14 17:46:46 +0000

    Upgrades to TypeScript 4.4.4

--- pages/api/weather/index.ts
+++ pages/api/weather/index.ts
@@ -29,6 +29,10 @@ export const handler: (_req: NextApiRequest, res: NextApiResponse) => Promise<vo
         }
         res.status(200).json(result);
     } catch (err) {
-        res.status(500).json({ statusCode: 500, message: err.message });
+        let errorMessage = 'Failed to load, error occurred!';
+        if (err instanceof Error) {
+            errorMessage = err.message;
+        }
+        res.status(500).json({ statusCode: 500, message: errorMessage });
     }
 };
