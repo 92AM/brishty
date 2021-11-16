5ca27828be1ad9575e60c7c3e2acd6331588f9bf
Arka Mitra <arka2k8@gmail.com>
2021-11-16 23:29:19 +0000

    Fixes typo

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -34,7 +34,7 @@ export default function PrivacyPolicy() {
                         </p>
                         <p>
                             <span className={'font-semibold'}>
-                                However, we may use consumer data and supply to Google in order display targeted Google
+                                However, we may use consumer data and supply to Google in order to display targeted Google
                                 advertisements on the website.
                             </span>
                         </p>
