bccb5562f331570189fd0fa011f95551595652cc
Arka Mitra <arka2k8@gmail.com>
2022-01-16 00:32:51 +0000

    Dynamic sitemap generation implementation (not super elegant, but does what I need it to do)

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,13 @@
 const siteUrl = 'https://www.brishty.net';
 
+const additionalWeatherSitemaps = [...Array(21).keys()].map(
+    (page) => `${siteUrl}/additional-sitemaps/server-sitemap-page-${page}.xml`,
+);
+
 module.exports = {
     siteUrl,
     generateRobotsTxt: true,
+    robotsTxtOptions: {
+        additionalSitemaps: [`${siteUrl}/sitemap.xml`, ...additionalWeatherSitemaps],
+    },
 };
---
+++ pages/additional-sitemaps/server-sitemap-page-20.xml/index.tsx
@@ -0,0 +1,10 @@
+import { GetServerSideProps } from 'next';
+import { getServerSideSitemap } from 'next-sitemap';
+import { generateDynamicWeatherSitemapFields } from '../../../services/DynamicSitemapGenerator';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const pageNumberToFetch = 19;
+    return getServerSideSitemap(ctx, [...(await generateDynamicWeatherSitemapFields(pageNumberToFetch))]);
+};
+
+export default function Site() {}
