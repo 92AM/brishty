a5964fccdc7b47e1c93fe756e4a2751a6b37ece1
Arka Mitra <arka2k8@gmail.com>
2022-01-16 02:50:20 +0000

    Fixes build failure

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,20 +1,18 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 
-const {
-    SITE_URL,
-    DYNAMIC_ADDITIONAL_SITEMAP_PATH,
-    SITEMAP_XML_EXTENSION,
-    STATIC_SITEMAP_FILE_NAME,
-} = require('./utility/constants');
+const staticSitemapFileName = 'sitemap.xml';
+const sitemapXmlExtension = '.xml';
+const dynamicAdditionalSitemapPath = '/additional-sitemaps/weather-sitemap-page-';
+const siteUrl = 'https://www.brishty.net';
 
 const additionalWeatherSitemaps = [...Array(21).keys()].map(
-    (page) => `${SITE_URL}${DYNAMIC_ADDITIONAL_SITEMAP_PATH}${page + 1}${SITEMAP_XML_EXTENSION}`,
+    (page) => `${siteUrl}${dynamicAdditionalSitemapPath}${page + 1}${sitemapXmlExtension}`,
 );
 
 module.exports = {
-    SITE_URL,
+    SITE_URL: siteUrl,
     generateRobotsTxt: true,
     robotsTxtOptions: {
-        additionalSitemaps: [`${SITE_URL}/${STATIC_SITEMAP_FILE_NAME}`, ...additionalWeatherSitemaps],
+        additionalSitemaps: [`${siteUrl}/${staticSitemapFileName}`, ...additionalWeatherSitemaps],
     },
 };
