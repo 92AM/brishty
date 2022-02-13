aa5695d829dc5c2eed8dc27068ac770ba20a8b3e
Arka Mitra <arka2k8@gmail.com>
2022-02-13 19:26:29 +0000

    Collapse Google Ad slot when ad fails to load

--- components/GoogleAdComponent.tsx
+++ components/GoogleAdComponent.tsx
@@ -1,13 +1,21 @@
-import React, { useEffect } from 'react';
-import { getWindow } from '../services/BrowserService';
+import React, { useEffect, useState } from 'react';
+import { getDocument, getWindow } from '../services/BrowserService';
+
+const googleAdsContainerName = 'adsbygoogle';
 
 export const GoogleAdComponent = () => {
+    const [shouldCollapseAdComponent, setShouldCollapseAdComponent] = useState(false);
+
     useEffect(() => {
         (getWindow().adsbygoogle = getWindow().adsbygoogle || []).push({});
     }, []);
 
-    return (
-        <div className="items-center justify-center bg-gray-800 border-b border-gray-100 p-8">
+    useEffect(() => {
+        setShouldCollapseAdComponent(getDocument().getElementsByClassName(googleAdsContainerName)?.length <= 1);
+    });
+
+    return shouldCollapseAdComponent ? null : (
+        <div className="items-center justify-center bg-gray-800 border-b border-gray-100 p-4">
             <div className="max-w-xl mx-auto max-w-screen-xl">
                 <div className="overflow-x-scroll hide-scroll-bar ">
                     <ins
