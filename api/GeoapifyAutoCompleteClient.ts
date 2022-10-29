8c164451bac521c12e30b5fa97147bb63407c85c
Arka Mitra <arka2k8@gmail.com>
2022-10-29 19:57:27 +0100

    Fix lint issue

--- api/GeoapifyAutoCompleteClient.ts
+++ api/GeoapifyAutoCompleteClient.ts
@@ -3,7 +3,6 @@ import { geoApiFyApiKey } from '../services/ApplicationEnvironmentConfigService'
 const URL = `https://api.geoapify.com/v1/geocode/autocomplete`;
 
 export const geoApiFyAutoCompleteClient = async (searchTerm: string): Promise<any> => {
-
     const query = 'text=' + searchTerm + '&format=json&apiKey=' + geoApiFyApiKey;
 
     return await fetch(URL + '?' + query);
