448093fc4ab13b11424c1eb4e7c127f010fbc358
Arka Mitra <arka2k8@gmail.com>
2022-10-29 19:53:17 +0100

    Removing caches location search

--- api/GeoapifyAutoCompleteClient.ts
+++ api/GeoapifyAutoCompleteClient.ts
@@ -1,13 +1,10 @@
 import { geoApiFyApiKey } from '../services/ApplicationEnvironmentConfigService';
-import { fetchResponseFromClientOrCache } from '../services/CacheService';
 
 const URL = `https://api.geoapify.com/v1/geocode/autocomplete`;
 
 export const geoApiFyAutoCompleteClient = async (searchTerm: string): Promise<any> => {
-    // 10 minutes
-    const cacheExpiryInMillisecond = 600000;
 
     const query = 'text=' + searchTerm + '&format=json&apiKey=' + geoApiFyApiKey;
 
-    return await fetchResponseFromClientOrCache(URL + '?' + query, cacheExpiryInMillisecond);
+    return await fetch(URL + '?' + query);
 };
