3632de50a419f908064dbecc059e0edd366bd8ab
Arka Mitra <arka2k8@gmail.com>
2023-03-12 20:58:39 +0000

    Adds api tests

--- services/test/WorldNewsService.spec.ts
+++ services/test/WorldNewsService.spec.ts
@@ -1,6 +1,6 @@
 import { getWorldNews } from '../WorldNewsService';
 import { nyTimesNewsClient } from '../../apis/NyTimesNewsClient';
-import { WORLD_NEW_CLIENT_RESPONSE, WORLD_NEWS_PARSED_MOCK } from '../../test-support/Stubs';
+import { WORLD_NEW_CLIENT_RESPONSE_MOCK, WORLD_NEWS_PARSED_MOCK } from '../../test-support/Stubs';
 
 jest.mock('../../apis/NyTimesNewsClient');
 
@@ -8,7 +8,7 @@ const nyTimesNewsClientMock = nyTimesNewsClient as jest.Mock;
 
 describe('WorldNewsService', () => {
     it('should get world news', async () => {
-        nyTimesNewsClientMock.mockResolvedValue(WORLD_NEW_CLIENT_RESPONSE);
+        nyTimesNewsClientMock.mockResolvedValue(WORLD_NEW_CLIENT_RESPONSE_MOCK);
 
         const result = await getWorldNews();
 
